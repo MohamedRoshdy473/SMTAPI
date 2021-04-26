b87030d7c2e35ac8e5f76a3c8d06ef7bd9b10106
MohamedRoshdy473 <60385152+MohamedRoshdy473@users.noreply.github.com>
2021-04-26 09:14:40 +0200

    API Latest

---
+++ SMT.Data/ViewModels/SearchOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Data.ViewModels
+{
+    public class SearchOptions
+    {
+        public int Year { get; set; }
+        public string ReportNumber { get; set; }
+        public String GridType { get; set; }
+        //public PaginationVm Pagination { get; set; }
+        public String Search { get; set; }
+        //public Filter Filter { get; set; }
+    }
+}
